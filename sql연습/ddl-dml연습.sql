b59730e79b495d0840c7cda1a2242476b036f07c
ansdud8239 <ansdud8239@naver.com>
2023-01-10 14:43:19 +0900

    dml & tx

--- sql연습/ddl-dml연습.sql
+++ sql연습/ddl-dml연습.sql
@@ -20,3 +20,33 @@ alter table member add self_intro text;
 
 alter table member drop juminbunho;
 
+-- insert
+insert into member values(null,'kickscar@gmail.com',password('1234'),'안대혁','개발팀',null);
+select  * from member;
+
+insert into member(no,email,name,dept,password)
+values(null,'kickscar2@gmail.com','안대혁','개발팀',password('1234'));
+select  * from member;
+
+-- update
+update member set email='kicksca3@gmail.com',password=password('5678') where no=2;
+
+-- delete
+delete from member where no=2;
+
+-- transaction
+-- 1 : 자동 커밋
+-- 0 : 커밋을 해줘야 반영 됨
+select @@autocommit;
+set autocommit=0;
+
+insert into member(no,email,name,dept,password)
+values(null,'kickscar5@gmail.com','안대혁5','개발팀5',password('1234'));
+
+commit;
+
+
+
+
+
+
