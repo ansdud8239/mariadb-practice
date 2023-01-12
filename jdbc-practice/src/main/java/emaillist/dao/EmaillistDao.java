c07d713faffcafe865baa725fcc9b02ce43ba790
ansdud8239 <ansdud8239@naver.com>
2023-01-12 14:56:42 +0900

    emaillist #

---
+++ jdbc-practice/src/main/java/emaillist/dao/EmaillistDao.java
@@ -0,0 +1,25 @@
+package emaillist.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import emaillist.vo.EmaillistVo;
+
+public class EmaillistDao {
+
+	public void insert(EmaillistVo vo) {
+	
+		
+	}
+
+	public List<EmaillistVo> findAll() {
+		List<EmaillistVo> result =new ArrayList<>();
+		
+		
+		return null;
+	}
+
+	
+	
+	
+}
