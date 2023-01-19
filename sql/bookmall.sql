0130eb401761f0abf2a7304a65be98534a9e8b29
ansdud8239 <ansdud8239@naver.com>
2023-01-19 16:59:28 +0900

    sql

--- sql/bookmall.sql
+++ sql/bookmall.sql
@@ -1,6 +1,8 @@
 -- user
 select * from user;
-
+select no from user where name=?
+-- category
+select * from category;
 -- cart
 select * from cart;
 
@@ -9,9 +11,14 @@ select * from book;
 
 -- orders
 select * from orders;
+select order_no,name,price,receive from orders where user_no=1;
 
 -- order_book
 select * from order_book;
+select b.no,b.name,a.count,c.name from order_book a 
+join book b on a.book_no=b.no
+join orders c on c.no=a.orders_no;
+
 select count(*) from cart where user_no=1;
 -- 회원의 cart를 구매했을 경우
 insert into order_book values(
@@ -25,6 +32,10 @@ select last_insert_id() as orders;
 -- 회원의 cart 에서 책 가격 구하기
 select count*price from cart a join book b on a.book_no=b.no where user_no=1;
 
--- auto 
+-- auto 조회
 SELECT AUTO_INCREMENT FROM information_schema.tables WHERE table_name = 'orders' AND table_schema = 'bookmall';
 
+-- auto increment 초기화
+ALTER TABLE orders AUTO_INCREMENT =1; 
+
+select concat((date_format(now(),'%Y%m%d')),'-',lpad(1,8,0)) from dual;
---
+++ sql/user.sql
@@ -0,0 +1,6 @@
+-- user
+desc user;
+select * from user;
+
+-- join
+insert into user values(null,'조문영','ansdud8239@naver.com',password('1234'),'female',now());
